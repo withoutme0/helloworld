fb3685dc1431a5df85ab8bbfc590a028081afb23
withoutme0 <64178674+withoutme0@users.noreply.github.com>
2020-04-23 02:10:29 +0000

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { createWorkerAddon } from "@watchedcom/sdk";
+import { directoryHandler, itemHandler } from "./handlers";
+const { scrapers } = require('source-scraper');
+export const filmizlevipAddon = createWorkerAddon({
+  id: "diziay.me",
+  name: "diziay",
+  version: "0.0.1",
+  homepage: "https://diziay.me/",
+  description: "Addon for diziay",
+  flags: {adult: false},
+  actions: ["directory", "item"],
+  itemTypes: ["movie","series"],
+  defaultDirectoryOptions: {
+    imageShape: "regular",
+    displayName: true
+  },
+  defaultDirectoryFeatures: {
+    search: { enabled: true}
+  },
+  dashboards: [
+    {
+      id: "orderpopular",
+      name: "Film: En Popüler"
+    },
+    {
+      id: "orderimdb",
+      name: "Film: IMDB Puana Göre"
+    },
+    {
+      id: "orderyear",
+      name: "Film: Yilina Göre"
+    },
+
+  ]
+});
+
+
+filmizlevipAddon.registerActionHandler("directory", directoryHandler);
+filmizlevipAddon.registerActionHandler("item", itemHandler);
+
